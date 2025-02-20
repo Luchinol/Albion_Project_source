e0cc4f2a5d69613acb27849b7887c10d60c9a338
Luis <luisantoniooc@gmail.com>
2025-02-20 18:06:38 -0300

    a

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 def main():
     # Ruta al archivo YAML; ajústala según tu estructura de carpetas.
-    data_path = "C:/Users/Lenovo/PycharmProjects/CNN_Project/data/dataset_roboflow/dataset_1/data.yaml"
+    data_path = "/data/dataset_roboflow/dataset_1/data.yaml"
     # Carga el modelo pre-entrenado, puedes variar a un modelo diferente (por ejemplo, yolov8s.pt)
     model = YOLO("yolov8n.pt")
 
